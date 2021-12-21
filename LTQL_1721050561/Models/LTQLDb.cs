745ab4ebd5b715575d452aff128629886f6456f7
HaiNamNg <89127631+HaiNamNg@users.noreply.github.com>
2021-12-21 13:25:11 +0700

    Add project files.

---
+++ LTQL_1721050561/Models/LTQLDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050561.Models
+{
+    public class LTQLDb :DbContext
+    {
+        public LTQLDb() : base("DbContext")
+        {
+
+        }
+    }
+    
+}
