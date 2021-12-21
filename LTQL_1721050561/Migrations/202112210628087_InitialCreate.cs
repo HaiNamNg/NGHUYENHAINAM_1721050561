7e94153dcbac9c69a2621d835a205a113ad03ad0
HaiNamNg <89127631+HaiNamNg@users.noreply.github.com>
2021-12-21 13:30:16 +0700

    NguyenHaiNam_1721050561

---
+++ LTQL_1721050561/Migrations/202112210628087_InitialCreate.cs
@@ -0,0 +1,16 @@
+﻿namespace LTQL_1721050561.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
